859276e94b5420bc39e7feacd0385d967a79890b
Pedro1043 <pedro14205x3@gmail.com>
2023-08-18 01:58:54 -0500

    Ejercicio 2 resuelto

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-
-
+        System.out.println("Ingrese la edad del usuario:");
+        Scanner input = new Scanner(System.in);
+        int edad = input.nextInt();
+        System.out.println("Ingrese 1 si el invitado trajo regalo; caso contrario, inserte 0:");
+        int gif = input.nextInt(); //1 si trajo regalo - 0 no trajo regalo
+        if(edad>=15 && gif ==1){
+            System.out.println("El invitado si puede ingresar a la fiesta");
+        } else if (edad < 15) {
+            System.out.println("El invitado si puede ingresar a la fiesta");
+        }else{
+            System.out.println("El invitado no puede ingresar");
+        }
+        input.close();
     }
 }
