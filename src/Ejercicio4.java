020375ab0c973c536e87526f2aac16076cbe4947
Pedro1043 <pedro14205x3@gmail.com>
2023-08-25 01:44:01 -0500

    Fix en el ejercicio4, solo pedia imprimir los números impares, no primos.

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -6,29 +6,12 @@ public class Ejercicio4 {
         int n = input.nextInt();
         int contador1 = 1;
         while(contador1<=n){
-            if(esPrimo(contador1) && esInpar(contador1)){
+            if(esInpar(contador1)){
                 System.out.println(contador1);
             }
             contador1++;
         }
         input.close();
-    }
-    public static boolean esPrimo(int numero){
-        /*if(numero <= 1){
-            return false;
-        }*/
-        int contador = 1;
-        while(contador <= numero){
-            if(numero % contador == 0){
-                if(numero == contador){
-                    return true;
-                }else if(contador != 1){
-                    return false;
-                }
-            }
-            contador++;
-        }
-        return false;
     }
     public static boolean esInpar(int numero){
         return numero % 2 != 0;
